5e4f6fd616750ca581dcce43dfd18e398f6e84c7
Alex Laursen <ajlaursen@aol.com>
2020-10-15 16:44:55 -0400

    finished readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # homwork-5-laursen
+
+* created day scheduler.
+
+* changed format from homework preview to be more modern
+
+* uses local storage to save and recall events
+
+* uses moment.js to retreive time data to show where in the day you are. 
+
+[screenshot](Assets/screenshot.png)
+
---
+++ script.js
@@ -0,0 +1,40 @@
+console.log(moment().format("H"));
+var currentTime = moment().format("H");
+var fullCurrentTimeDate = moment().format("MMMM Do YYYY, h:mm a");
+var userEntry;
+userEntry = JSON.parse(localStorage.getItem("userEntry")) || [];
+
+// adding css classes
+for (i = 8; i < 18; i++) {
+  if (currentTime < i) {
+    $("#" + i).addClass("future");
+  }
+  if (currentTime == i) {
+    $("#" + i).addClass("present");
+  }
+  if (currentTime > i) {
+    $("#" + i).addClass("past");
+  }
+  $("#" + i).text(userEntry[i - 8].text);
+}
+
+// saving user text
+// homeowrk example shows one button per line and that seemed incredibly innefficent and ugly to me.
+// this funciton creates an array with user entry saved with the id of the text area
+
+$(".fa-save").click(function () {
+  userEntry = [];
+  for (i = 8; i < 18; i++) {
+    userEntry.push({
+      time: i,
+      text: $("#" + i)
+        .val()
+        .trim(),
+    });
+  }
+  console.log(userEntry);
+  localStorage.setItem("userEntry", JSON.stringify(userEntry));
+});
+
+// adding current date and time to top of page
+$("#currentDay").text(fullCurrentTimeDate);
